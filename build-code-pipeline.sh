a664c86ffb49f0411286ce95eea85db3097b8eaa
Nagaraj <shakthipav@gmail.com>
2025-01-25 19:41:35 +0000

    updated build

--- build-code-pipeline.sh
+++ build-code-pipeline.sh
@@ -2,9 +2,5 @@
 aws cloudformation create-stack \
   --stack-name dev-lambda-pipeline \
   --template-body file://lambda_code_pipeline.yaml \
-  --parameters ParameterKey=GitHubOwner,ParameterValue=Nagaraj104065 \
-               ParameterKey=GitHubRepo,ParameterValue=graphql-js-aws \
-               ParameterKey=GitHubOAuthToken,ParameterValue=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 \
-               ParameterKey=CodePipelineBucketPrefix,ParameterValue=dev-pos-lambda-pipeline \
-               ParameterKey=Environment,ParameterValue=dev \
+  --parameters file://dev.json \
   --capabilities CAPABILITY_NAMED_IAM
